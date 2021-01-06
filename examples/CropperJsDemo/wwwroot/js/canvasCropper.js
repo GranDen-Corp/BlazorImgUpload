73a6ed95b2fa2487cd5fb147a7d178b2d6db689a
Chen-Yu Pao <windperson@gmail.com>
2021-01-06 14:53:55 +0800

    Cropperjs demo

---
+++ examples/CropperJsDemo/wwwroot/js/canvasCropper.js
@@ -0,0 +1,40 @@
+﻿// noinspection JSUnusedGlobalSymbols
+export function initCropper(canvas, cropButton, restoreButton, resultContainer, fileInputId) {
+
+    let context = canvas.getContext("2d");
+    let cropper;
+
+    let fileInput = document.getElementById(fileInputId);
+    fileInput.addEventListener('change', function (event) {
+        let uploadFile = event.target.files[0];
+        if (uploadFile.type.match(/^image\//)) {
+            let img = new Image();
+            img.onload = function () {
+                // if (img.src !== null) {
+                //     URL.revokeObjectURL(img.src);
+                // }
+                context.canvas.height = img.height;
+                context.canvas.width = img.width;
+                context.drawImage(img, 0, 0);
+                cropper = new Cropper(canvas, {
+                        aspectRatio: 1,
+                        center: true
+                    });
+            };
+            img.src = URL.createObjectURL(uploadFile);
+        }
+    });
+
+    cropButton.addEventListener('click', function (e) {
+        const croppedImageDataURL = cropper.getCroppedCanvas().toDataURL('image/png');
+        const resultImg = new Image();
+        resultImg.src = croppedImageDataURL;
+        resultContainer.append(resultImg);
+    });
+
+    restoreButton.addEventListener('click', function (e) {
+        cropper.reset();
+        resultContainer.firstChild.remove();
+    });
+
+}
