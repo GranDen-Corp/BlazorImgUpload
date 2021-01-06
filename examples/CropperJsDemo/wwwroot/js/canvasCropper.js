ccabbf8d175d1779ade695953b7206b0280848c3
Chen-Yu Pao <windperson@gmail.com>
2021-01-06 18:05:23 +0800

    Fix some js error

--- examples/CropperJsDemo/wwwroot/js/canvasCropper.js
+++ examples/CropperJsDemo/wwwroot/js/canvasCropper.js
@@ -1,40 +1,56 @@
 ﻿// noinspection JSUnusedGlobalSymbols
 export function initCropper(canvas, cropButton, restoreButton, resultContainer, fileInputId) {
-
     let context = canvas.getContext("2d");
-    let cropper;
-
+    let cropper = null;
+    let previewImg = new Image();
+    let uploadMineType = 'image/png';
     let fileInput = document.getElementById(fileInputId);
-    fileInput.addEventListener('change', function (event) {
-        let uploadFile = event.target.files[0];
-        if (uploadFile.type.match(/^image\//)) {
-            let img = new Image();
-            img.onload = function () {
-                // if (img.src !== null) {
-                //     URL.revokeObjectURL(img.src);
-                // }
-                context.canvas.height = img.height;
-                context.canvas.width = img.width;
-                context.drawImage(img, 0, 0);
-                cropper = new Cropper(canvas, {
+
+    if (fileInput) {
+        fileInput.addEventListener('change', function (event) {
+            let uploadFile = event.target.files[0];
+            if (uploadFile.type.match(/^image\//)) {
+                uploadMineType = uploadFile.type;
+                previewImg.onload = function () {
+                    if (cropper !== null) {
+                        cropper.destroy();
+                        cropper = null;
+                    }
+                    context.clearRect(0, 0, canvas.width, canvas.height);
+                    context.canvas.height = previewImg.height;
+                    context.canvas.width = previewImg.width;
+                    context.drawImage(previewImg, 0, 0);
+                    cropper = new Cropper(canvas, {
                         aspectRatio: 1,
                         center: true
                     });
-            };
-            img.src = URL.createObjectURL(uploadFile);
-        }
-    });
+                };
+                if (previewImg.src) {
+                    URL.revokeObjectURL(previewImg.src);
+                }
+                previewImg.src = URL.createObjectURL(uploadFile);
+            }
+        });
+    }
 
     cropButton.addEventListener('click', function (e) {
-        const croppedImageDataURL = cropper.getCroppedCanvas().toDataURL('image/png');
+        if (!cropper) {
+            return;
+        }
+        const croppedImageDataURL = cropper.getCroppedCanvas().toDataURL(uploadMineType);
         const resultImg = new Image();
         resultImg.src = croppedImageDataURL;
         resultContainer.append(resultImg);
     });
 
     restoreButton.addEventListener('click', function (e) {
-        cropper.reset();
-        resultContainer.firstChild.remove();
+        if (cropper) {
+            cropper.reset();
+        }
+        let previewImg = resultContainer.firstChild;
+        if (previewImg) {
+            previewImg.remove();
+        }
     });
 
 }
